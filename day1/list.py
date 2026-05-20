8c6b383ba4c2a639e1afa33805b5fa02cfecb929
Arvind428 <arvind592006@gmail.com>
2026-05-20 11:53:48 +0530

    lists

---
+++ day1/list.py
@@ -0,0 +1,20 @@
+"""
+lists is an ordered sequence of elements that can be comprised of strings
+Lists are mutable
+use zero based index
+"""
+#index based accessing
+cities=["Los Angeles","London","Tokyo"]
+i=cities[0]
+print(i)#Los Angeles
+
+#negative index accessing
+n=cities[-1]
+print(n)#Tokyo
+
+#list constructor
+name="Arvind"
+lc=list(name)
+print(lc)
+
+#
