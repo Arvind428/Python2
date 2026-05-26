e54f3e4d260ef9c6e2bb3842b01329dc3ad3dd2a
Arvind428 <arvind592006@gmail.com>
2026-05-26 21:14:00 +0530

    free

--- day2/new.py
+++ day2/new.py
@@ -1,2 +1,3 @@
-x=25%4
-print(x)
+#Over the next few lessons we are going to learn about lists, tuples, and ranges, which are three basic sequence types used in Python.
+#The list data type is an ordered sequence of elements that can be comprised of strings, numbers, or even other lists. Lists are mutable and use zero-based indexing, meaning that the first element of the list is at index zero.
+
